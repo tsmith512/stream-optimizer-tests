1224f0a6cce95990094dc2de3764d2f5e1696bb5
Taylor Smith <tsmith@cloudflare.com>
2025-03-11 13:25:51 -0500

    Pick apart a different URL format to reconstruct a media transformations path

--- functions/videos/[file].js
+++ functions/videos/[file].js
@@ -3,7 +3,21 @@ export async function onRequest(context) {
 
   const url = new URL(request.url);
 
-  return new Response(JSON.stringify(url), {
-    headers: {'Content-Type': 'application/json'}
-  })
+  // Where my originals live
+  const storage = 'https://pub-9cf4bfca6e924401bd4ac87ca9174da6.r2.dev';
+
+  // Remove the first slash and the "videos" prefix
+  const file = url.pathname.substring(1).split('/');
+  console.log(file)
+  file.shift();
+
+  // Get width from query param or use default
+  const width = url.searchParams.get('width') ?? 400;
+
+  // Reconstruct Media Transformations URL
+  const dest = `${url.origin}/cdn-cgi/media/width=${width}/${storage}/${file.join('/')}`;
+
+  return new Response(dest, {
+    headers: {'Content-Type': 'text/plain'}
+  });
 }
