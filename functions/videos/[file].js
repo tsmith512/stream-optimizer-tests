00d986ec4e6f917dcc1d047cfcad47defbcf246f
Taylor Smith <tsmith@cloudflare.com>
2025-03-11 16:57:55 -0500

    a theory...

--- functions/videos/[file].js
+++ functions/videos/[file].js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
   const width = url.searchParams.get('width') ?? 400;
 
   // Reconstruct Media Transformations URL
-  const dest = `${url.origin}/cdn-cgi/media/width=${width}/${storage}/${file.join('/')}`;
+  const dest = `https://tsmith.com/cdn-cgi/media/width=${width}/${storage}/${file.join('/')}`;
   console.log(dest)
 
   // An example request to https://scratchwork.tsmith.net/videos/journal.mp4?width=300
@@ -22,9 +22,8 @@ export async function onRequest(context) {
   // generates the URL https://scratchwork.tsmith.net/cdn-cgi/media/width=300/https://pub-9cf4bfca6e924401bd4ac87ca9174da6.r2.dev/journal.mp4
   // Which I can hit directly
   // But this doesn't work:
-  return fetch(`https://scratchwork.tsmith.net/cdn-cgi/trace`, {
+  return fetch(dest, {
     "headers": {
-      "Cf-Trace-Id": "000e90ee8d84ae64:e90ee8d84ae64:0:3:1741728367:7fdb4824f2ff8be7c3fd2da23a302c2b84041690c37859ebc97cad5f1c4e59ed",
     }
   });
 }
